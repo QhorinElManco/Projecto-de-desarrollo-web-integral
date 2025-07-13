ae5fc6f0a1fa5cff1778f7c7a18ac25fbc5f412e
Maynor Pineda <sbethuell@gmail.com>
2025-07-13 15:16:41 -0600

    Replace dynamic alias generation with static aliases and update dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import {getViteAliases} from "./config/aliases.ts";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     resolve: {
-        alias: getViteAliases()
+        alias: {
+            "@components/*": "src/components/*",
+            "@pages/*": "src/pages/*",
+            "@stores/*": "src/stores/*",
+            "@hooks/*": "src/hooks/*",
+            "@utils/*": "src/utils/*",
+            "@data/*": "src/data/*",
+            // 'swiper/css': 'swiper/swiper.min.css',
+            // 'swiper/css/effect-coverflow': 'swiper/modules/effect-coverflow/effect-coverflow.min.css',
+            // 'swiper/css/pagination': 'swiper/modules/pagination/pagination.min.css',
+        }
     }
 })
